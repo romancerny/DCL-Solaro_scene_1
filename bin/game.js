655878b9e562affab43347c064e9784632534f76
Richard Cerny <rcerny007@gmail.com>
2020-12-20 22:28:36 +0000

    4th commit

--- bin/game.js
+++ bin/game.js
@@ -56,43 +56,98 @@ const transform5 = new Transform({
 })
 entity4.addComponentOrReplace(transform5)
 
-const caribbeanWater = new Entity('caribbeanWater')
-engine.addEntity(caribbeanWater)
-caribbeanWater.setParent(_scene)
+const whitetile = new Entity('whitetile')
+engine.addEntity(whitetile)
+whitetile.setParent(_scene)
 const transform6 = new Transform({
-  position: new Vector3(8, 0, 8),
+  position: new Vector3(16, 0, 16),
   rotation: new Quaternion(0, 0, 0, 1),
   scale: new Vector3(1, 1, 1)
 })
-caribbeanWater.addComponentOrReplace(transform6)
-const gltfShape2 = new GLTFShape("models/WaterPatchFull_01/WaterPatchFull_01.glb")
+whitetile.addComponentOrReplace(transform6)
+const gltfShape2 = new GLTFShape("models/whiteTile.glb")
 gltfShape2.withCollisions = true
 gltfShape2.isPointerBlocker = true
 gltfShape2.visible = true
-caribbeanWater.addComponentOrReplace(gltfShape2)
+whitetile.addComponentOrReplace(gltfShape2)
 
-const floorCreamSmall = new Entity('floorCreamSmall')
-engine.addEntity(floorCreamSmall)
-floorCreamSmall.setParent(_scene)
+const whitetile2 = new Entity('whitetile2')
+engine.addEntity(whitetile2)
+whitetile2.setParent(_scene)
 const transform7 = new Transform({
-  position: new Vector3(32, 0, 32),
+  position: new Vector3(16, 0, 16),
   rotation: new Quaternion(0, 0, 0, 1),
-  scale: new Vector3(8, 1, 3.5)
+  scale: new Vector3(1, 1, 1)
 })
-floorCreamSmall.addComponentOrReplace(transform7)
-const gltfShape3 = new GLTFShape("models/CreamFloor_Small.glb")
+whitetile2.addComponentOrReplace(transform7)
+whitetile2.addComponentOrReplace(gltfShape2)
+
+const whitetile3 = new Entity('whitetile3')
+engine.addEntity(whitetile3)
+whitetile3.setParent(_scene)
+const transform8 = new Transform({
+  position: new Vector3(16, 0, 16),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+whitetile3.addComponentOrReplace(transform8)
+whitetile3.addComponentOrReplace(gltfShape2)
+
+const whitetile4 = new Entity('whitetile4')
+engine.addEntity(whitetile4)
+whitetile4.setParent(_scene)
+const transform9 = new Transform({
+  position: new Vector3(16, 0, 16),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+whitetile4.addComponentOrReplace(transform9)
+whitetile4.addComponentOrReplace(gltfShape2)
+
+const whitetile5 = new Entity('whitetile5')
+engine.addEntity(whitetile5)
+whitetile5.setParent(_scene)
+const transform10 = new Transform({
+  position: new Vector3(17, 0, 15),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(5, 1, 4.999999523162842)
+})
+whitetile5.addComponentOrReplace(transform10)
+const gltfShape3 = new GLTFShape("models/whiteTile.glb")
 gltfShape3.withCollisions = true
 gltfShape3.isPointerBlocker = true
 gltfShape3.visible = true
-floorCreamSmall.addComponentOrReplace(gltfShape3)
+whitetile5.addComponentOrReplace(gltfShape3)
 
-const floorCreamSmall2 = new Entity('floorCreamSmall2')
-engine.addEntity(floorCreamSmall2)
-floorCreamSmall2.setParent(_scene)
-floorCreamSmall2.addComponentOrReplace(gltfShape3)
-const transform8 = new Transform({
-  position: new Vector3(32, 0, 28),
+const whitetile6 = new Entity('whitetile6')
+engine.addEntity(whitetile6)
+whitetile6.setParent(_scene)
+whitetile6.addComponentOrReplace(gltfShape3)
+const transform11 = new Transform({
+  position: new Vector3(15, 0, 15),
   rotation: new Quaternion(0, 0, 0, 1),
-  scale: new Vector3(8, 1, 7)
+  scale: new Vector3(5, 1, 4.999999523162842)
 })
-floorCreamSmall2.addComponentOrReplace(transform8)
+whitetile6.addComponentOrReplace(transform11)
+
+const whitetile7 = new Entity('whitetile7')
+engine.addEntity(whitetile7)
+whitetile7.setParent(_scene)
+whitetile7.addComponentOrReplace(gltfShape3)
+const transform12 = new Transform({
+  position: new Vector3(15, 0, 17),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(5, 1, 4.999999523162842)
+})
+whitetile7.addComponentOrReplace(transform12)
+
+const whitetile8 = new Entity('whitetile8')
+engine.addEntity(whitetile8)
+whitetile8.setParent(_scene)
+whitetile8.addComponentOrReplace(gltfShape3)
+const transform13 = new Transform({
+  position: new Vector3(17, 0, 17),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(5, 1, 4.999999523162842)
+})
+whitetile8.addComponentOrReplace(transform13)
